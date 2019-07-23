59eabc4b139ff6ba793fa6a87e2e68f3e80cc0e1
Illia Holovii <goloviyillya@gmail.com>
2019-07-23 15:26:24 +0300

    configuration webpack

--- promise.js
+++
@@ -1,9 +0,0 @@
-const arrPromise = [];
-
-for (let i = 1; i <= 10; i++) {
-  arrPromise[i-1] = fetch(`https://jsonplaceholder.typicode.com/users/${i}`).then(res => res);
-}
-
-Promise.all(arrPromise).then(result => {
-  console.log(result);
-})

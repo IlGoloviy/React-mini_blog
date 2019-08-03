467ae4c073c62bfa4b84dab3b48715301d3dd4b4
Illia Holovii <goloviyillya@gmail.com>
2019-08-03 10:49:06 +0300

    added to his user his posts

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import Post from '../components/Post';
+import User from '../components/User';
 import axios from 'axios';
 
 export default class UserPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      post: null
+      post: null,
+      user: null
     }
   }
   
   render() {
+    
     return (
       <div className="post-page">
-        {this.state.post && <Post post={this.state.post}/>}
+        {this.state.post && <Post post={this.state.post} />}
+        {this.state.user && 
+          <div>
+            <h3>author of this post</h3>
+            <User user={this.state.user} />
+          </div>}
       </div>
     );
   }
@@ -22,5 +30,14 @@ export default class UserPage extends React.Component {
     axios.get(`http://jsonplaceholder.typicode.com/posts/${this.props.params.postId}`).then(res => {
       this.setState({post: res.data});
     });
+  }
+
+  componentDidUpdate() {
+    const idUser = this.state.post.userId;
+    if (idUser) {
+      axios.get(`http://jsonplaceholder.typicode.com/users/${idUser}`).then(res => {
+        this.setState({user: res.data});
+      });
+    }
   }
 }

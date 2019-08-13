792d5e27025b25a20b8ed823345b0f3ca7b5ef43
Illia Holovii <goloviyillya@gmail.com>
2019-08-13 13:27:38 +0300

    edit component Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,49 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from '../components/Menu';
 import MenuItem from '../components/MenuItem';
 import routes from '../routes';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-class Layout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.src = "https://i.pravatar.cc/75?img=2";
-    this.click = this.click.bind(this);
-  }
+const Layout = props => {
+  const [active, setActive] = useState({});
+  const src = "https://i.pravatar.cc/75?img=2";
   
-  isActive(href) {
+  function isActive(href) {
     return window.location.pathname === href;
   }
 
-  click() {
-    this.setState({active: null});
+  const click = () => {
+    setActive({active: null});
   }
 
-  render() {
-    return (
-      <>
-      <div onClick={this.click}>
-      <Menu src={this.src}>
-        <MenuItem href="/" active={this.isActive("/")}>Main</MenuItem>
-        <MenuItem href="/users" active={this.isActive("/users")}>Users</MenuItem>
-        <MenuItem href="/posts" active={this.isActive("/posts")}>Posts</MenuItem>
-        <MenuItem href="/comments" active={this.isActive("/comments")}>Comments</MenuItem>
-      </Menu>
-      </div>
-      <div className="container">
-        <Switch>
-          {routes.map((route, key) => {
-            return <Route key={key} {...route} />
-          })}
-        </Switch>
-      </div>
-      <footer className="footer">
-        &copy; 2019 blog Malysha
-      </footer>
-      </>
-    );
-  }
+  return (
+    <>
+    <div onClick={click}>
+    <Menu src={src}>
+      <MenuItem href="/" active={isActive("/")}>Main</MenuItem>
+      <MenuItem href="/users" active={isActive("/users")}>Users</MenuItem>
+      <MenuItem href="/posts" active={isActive("/posts")}>Posts</MenuItem>
+      <MenuItem href="/comments" active={isActive("/comments")}>Comments</MenuItem>
+    </Menu>
+    </div>
+    <div className="container">
+      <Switch>
+        {routes.map((route, key) => {
+          return <Route key={key} {...route} />
+        })}
+      </Switch>
+    </div>
+    <footer className="footer">
+      &copy; 2019 blog Malysha
+    </footer>
+    </>
+  );
 }
 
 export default Layout;
